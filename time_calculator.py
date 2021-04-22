09a8ba6a3b6901fe04aff5057cbb2728bdceb4ef
jmkimani <mainajkimani@gmail.com>
2021-04-22 23:08:24 +0300

    Final version.

--- time_calculator.py
+++ time_calculator.py
@@ -11,13 +11,13 @@ def add_time(start, duration, day = None):
         day (str, optional): A starting day of the week.
 
     Returns:
-        new_time (str): The time the duration ends.
+        (str): The time the duration ends.
     """
     start_hr, _s = start.split(":")
     start_min, period = _s.split(" ")
     duration_hr, duration_min = duration.split(":")
 
-    # str() => int()
+    # str => int
     start_hr = int(start_hr)
     start_min = int(start_min)
     duration_hr = int(duration_hr)
@@ -27,7 +27,6 @@ def add_time(start, duration, day = None):
     # Add 12 hours to `start_hr` if it is past noon.
     if period == "PM" and start_hr != 12:
         start_hr = start_hr + 12
-
     # 12:00 AM midnight is 00:00 HRS.
     if period == "AM" and start_hr == 12:
         start_hr = 0
@@ -37,38 +36,37 @@ def add_time(start, duration, day = None):
 
     # Increment hours if min > 59
     end_hr += end_min // 60
-
     # Minutes have a max val of 59
     end_min = end_min % 60
-
+    # Count days
     num_days = end_hr // 24
     # Final hour is within 24 HRs
     end_hr = end_hr % 24
 
     # Formatting the return string.
+    # AM / PM
+    # 00:00 - 11:59HR is AM
+    if end_hr < 12:
+        p = "AM"
+        # 00:00 midnight is 12:00 AM
+        if end_hr == 0:
+            end_hr = 12
+    # 13:00 - 23:59HR is PM
+    elif end_hr > 12:
+        p = "PM"
+        end_hr -= 12
+    # 12:00 noon is PM
+    elif end_hr == 12:
+        p = "PM"
+        end_hr = 12
     # "0" padding for minutes < 10
     if end_min < 10:
         min = "0" + str(end_min)
     else:
         min = str(end_min)
-
-    # AM / PM
-    if end_hr < 12:
-        p = "AM"
-        if end_hr == 0:
-            end_hr = 12
-    elif end_hr > 12:
-        p = "PM"
-        end_hr -= 12
-    elif end_hr == 12:
-        p = "PM"
-        end_hr = 12
-
     hr = str(end_hr)
 
     result = hr + ":" + min + " " + p
-
-    # Tests: [1, ]
     if not day:
         # Same day
         if num_days == 0:
@@ -82,8 +80,6 @@ def add_time(start, duration, day = None):
         else:
             return result + " (" + str(num_days) + " days later)"
 
-    # Tests: []
-
     days = {
         0: "Monday",
         1: "Tuesday",
@@ -93,31 +89,18 @@ def add_time(start, duration, day = None):
         5: "Saturday",
         6: "Sunday"
     }
-
     final_day = None
-
     for k, d in days.items():
         if d == day.capitalize():
             final_day = days[(k + num_days) % 7]
 
     result += ", " + final_day
-
     # Same day
     if num_days == 0:
         return result
-
     # The next day
     elif num_days == 1:
         return result + " (next day)"
-
     # `n` days later
     else:
         return result + " (" + str(num_days) + " days later)"
-    return
-
-# add_time("3:00 PM", "3:10")
-# print(add_time("11:30 AM", "2:32", "Monday"))
-# add_time("9:15 PM", "5:30")
-# add_time("12:59 PM", "24:05")
-# add_time("12:59 AM", "24:05")
-# print(add_time("11:59 PM", "24:05"))
